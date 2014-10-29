e9919da15948456f7030cadf96ecd71aa4821a48
alex <alex@cobrowser.net>
2014-10-29 14:31:30 +0100

    added php5enmod

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,11 +1,17 @@
 class xdebug::debian {
 
     include xdebug::params
-    
+
     package { "xdebug":
         name   => $xdebug::params::pkg,
         ensure => installed,
         require => Package['php5-cli'],
+    }
+
+    exec { 'enable_xdebug':
+      command => 'php5enmod xdebug && php5enmod -s cli xdebug',
+      user => root,
+      require => [Package['php5-cli'],
     }
 
 }
